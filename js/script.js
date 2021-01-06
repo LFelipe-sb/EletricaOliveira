f20ef87a8c5681013b3f2aa68da46aedba2b62bc
Luis Felipe <luisfelipe.sb@outlook.com>
2021-01-06 14:42:15 -0300

    Adicionando metatag de SEO

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 function AtivandoScrollSuavizado(){
-    const linksInternos = document.querySelectorAll('.menu-nav ul li a[href^="#"]');
+    const linksInternos = document.querySelectorAll('.menu-bg .menu .menu-nav ul li a[href^="#"]');
+    // let linksInternos = document.getElementById('sobre')
     function rolandoSuave(event){
         event.preventDefault();
         const href = event.currentTarget.getAttribute('href');
         const section = document.querySelector(href);
-        console.log(section);
         section.scrollIntoView({
             behavior: 'smooth',
             block: 'start',
